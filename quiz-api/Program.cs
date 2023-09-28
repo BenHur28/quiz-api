c134bee6f4f98dead246f41d5917aa309f4a2c25
Ben Hur <bhur777@gmail.com>
2023-09-28 03:56:12 -0400

    Add CORS

--- quiz-api/Program.cs
+++ quiz-api/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(buil
 
 var app = builder.Build();
 
+app.UseCors(options => options.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
